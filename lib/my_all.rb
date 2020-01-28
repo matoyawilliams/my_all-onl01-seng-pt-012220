e931ca27326b4ebb8ba2f87988b07f80ef656e21
Matoya Williams <matoyawilliams@yahoo.com>
2020-01-28 20:23:53 +0000

    Automatically backed up by Learn

--- lib/my_all.rb
+++ lib/my_all.rb
@@ -3,8 +3,9 @@ require 'pry'
 def my_all?(collection)
 i = 0 
 while i < 
-collection.length 
-yield (collecction [i])
+collection.length
+block_return_values <<
+yield (collection [i])
 i = i + l
 end
 end
